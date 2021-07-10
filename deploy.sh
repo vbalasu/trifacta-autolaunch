9f89da12310dd2a75df6e0f31524a134529a1ebb
vbalasu <vbalasu@gmail.com>
2021-07-10 16:50:21 -0700

    Updated package and deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 export AWS_PROFILE=default
-time chalice deploy
+aws cloudformation deploy --template-file trifacta-packaged.yaml --stack-name trifacta-stack --capabilities CAPABILITY_IAM
--- package.sh
+++ package.sh
@@ -1,1 +1,5 @@
 chalice package --merge-template app-stack.yaml out
+aws cloudformation package \
+     --template-file out/sam.yaml \
+     --s3-bucket trifacta-autolaunch-us-west-2 \
+     --output-template-file trifacta-packaged.yaml
