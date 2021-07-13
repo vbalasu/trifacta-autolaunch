c1a3ee5d99c85be385299205116a02d3bac17bac
vbalasu <vbalasu@gmail.com>
2021-07-13 05:17:14 -0700

    Copy to additional region (us-east-1)

--- package.sh
+++ package.sh
@@ -7,3 +7,7 @@ aws cloudformation package \
 export CODE_URI=$(cat trifacta-packaged.yaml | yq -r .Resources.APIHandler.Properties.CodeUri)
 export KEY=$(echo $CODE_URI | sed -e 's/s3:\/\/trifacta-autolaunch-us-west-2\///')
 aws s3api put-object-acl --acl public-read --bucket trifacta-autolaunch-us-west-2 --key $KEY
+
+# Copy to additional regions
+export REGION=us-east-1
+aws s3 cp --acl public-read s3://trifacta-autolaunch-us-west-2/$KEY s3://trifacta-autolaunch-$REGION/$KEY
