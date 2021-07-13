f8c309410533421bffc15b7bacd4154ed287b35b
vbalasu <vbalasu@gmail.com>
2021-07-12 18:51:39 -0700

    Granted public-read access to Lambda CodeURI

--- package.sh
+++ package.sh
@@ -1,5 +1,9 @@
+export AWS_PROFILE=default
 chalice package --merge-template trifacta-enterprise-7.6.1-existing-vpc.mod.yaml out
 aws cloudformation package \
      --template-file out/sam.yaml \
      --s3-bucket trifacta-autolaunch-us-west-2 \
      --output-template-file trifacta-packaged.yaml
+export CODE_URI=$(cat trifacta-packaged.yaml | yq -r .Resources.APIHandler.Properties.CodeUri)
+export KEY=$(echo $CODE_URI | sed -e 's/s3:\/\/trifacta-autolaunch-us-west-2\///')
+aws s3api put-object-acl --acl public-read --bucket trifacta-autolaunch-us-west-2 --key $KEY
