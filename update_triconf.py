f0461121f603eb73ab198a992a5c81c51241209e
vbalasu <vbalasu@gmail.com>
2021-07-14 16:23:47 -0700

    Added update_triconf.py with aws.s3.bucket.name

---
+++ update_triconf.py
@@ -0,0 +1,17 @@
+# coding: utf-8
+path_to_triconf = 'trifacta-conf.json'
+
+import shutil, datetime
+timestamp = datetime.datetime.now().isoformat().replace(':', '_')
+path_to_backup = path_to_triconf + '.' + timestamp
+shutil.copyfile(path_to_triconf, path_to_backup)
+import json
+with open(path_to_triconf) as f:
+    config = json.load(f)
+    
+import os
+os.getenv('TRIFACTA_BUCKET')
+config['aws']['s3']['bucket']['name'] = os.getenv('TRIFACTA_BUCKET')
+with open(path_to_triconf, 'w') as f:
+    json.dump(config, f, indent=2)
+    
