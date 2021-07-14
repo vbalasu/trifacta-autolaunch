5c0df2d0d9344c246fcc59d68d9e295b82bfa8e6
vbalasu <vbalasu@gmail.com>
2021-07-14 14:43:29 -0700

    autolaunch script - added ability to connect via ssh

--- autolaunch.sh
+++ autolaunch.sh
@@ -5,3 +5,6 @@ export TRIFACTA_URL=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKe
 export TRIFACTA_INSTANCE_ID=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "TrifactaInstanceId").OutputValue' -r)
 export TRIFACTA_BUCKET=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "TrifactaBucket").OutputValue' -r)
 export EMR_CLUSTER_ID=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "EmrClusterId").OutputValue' -r)
+export TRIFACTA_PUBLIC_DNS_NAME=$(aws ec2 describe-instances --instance-ids $TRIFACTA_INSTANCE_ID |jq '.Reservations[].Instances[].PublicDnsName' -r)
+export PATH_TO_KEY="/Users/vbalasubramaniam/Desktop/amazon/vbalasubramaniam-keypair-2.pem"
+ssh -i $PATH_TO_KEY centos@$TRIFACTA_PUBLIC_DNS_NAME "hostname"
