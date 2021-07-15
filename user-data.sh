6e669b25b378d2c1258edd92ca79d19ef0f73ed9
vbalasu <vbalasu@gmail.com>
2021-07-15 15:14:13 -0700

    Moved generate token to after trifacta restart, redirect user-data output to /var/log/user-data.log

--- user-data.sh
+++ user-data.sh
@@ -1,9 +1,11 @@
 # BEGIN Cloudformation template UserData section
-#yum update -y
-#yum install git jq -y
-#cd /root
-#git clone https://github.com/vbalasu/trifacta-autolaunch.git
-#trifacta-autolaunch/user-data.sh
+#            #!/bin/bash -xe
+#            exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+#            yum update -y
+#            yum install -y git jq
+#            cd /root
+#            git clone https://github.com/vbalasu/trifacta-autolaunch.git
+#            trifacta-autolaunch/user-data.sh
 # END Cloudformation template UserData section
 cd /root
 export TRIFACTA_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
@@ -15,15 +17,16 @@ export TRIFACTA_URL=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKe
 export TRIFACTA_BUCKET=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "TrifactaBucket").OutputValue' -r)
 export EMR_CLUSTER_ID=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "EmrClusterId").OutputValue' -r)
 
-curl -s -X POST -H "Content-Type: application/json" -d '{"lifetimeSeconds":86400, "description":"Autolaunch 1 day token"}' -u admin@trifacta.local:$TRIFACTA_INSTANCE_ID $TRIFACTA_URL/v4/apiAccessTokens >token.json
-export TRIFACTA_TOKEN=$(cat token.json | jq '.tokenValue' -r)
-
 # Set aws.s3.enabled to true using configuration service
 curl -s -X PUT -H 'Content-Type: application/json' --data 'true' localhost:10075/v1/setting/aws.s3.enabled/system/1
 
 # Update triconf
 python3 trifacta-autolaunch/update_triconf.py
 service trifacta restart
+
+# Obtain token
+curl -s -X POST -H "Content-Type: application/json" -d '{"lifetimeSeconds":86400, "description":"Autolaunch 1 day token"}' -u admin@trifacta.local:$TRIFACTA_INSTANCE_ID $TRIFACTA_URL/v4/apiAccessTokens >token.json
+export TRIFACTA_TOKEN=$(cat token.json | jq '.tokenValue' -r)
 
 # Configure EMR
 curl -s -X POST -H "Content-Type: application/json" -d "{\"emrClusterId\": \"$EMR_CLUSTER_ID\", \"resourceBucket\": \"$TRIFACTA_BUCKET\", \"resourcePath\": \"resources\"}" -H "Authorization: Bearer $TRIFACTA_TOKEN" $TRIFACTA_URL/v4/emrClusters
