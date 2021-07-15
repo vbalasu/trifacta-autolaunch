6dfba7b82e6f1e31137c93827774b536cd0568c0
vbalasu <vbalasu@gmail.com>
2021-07-15 10:43:57 -0700

    Added user-data.sh

---
+++ user-data.sh
@@ -0,0 +1,26 @@
+yum update -y
+yum install git jq -y
+git clone https://github.com/vbalasu/trifacta-autolaunch.git
+export TRIFACTA_INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+export AWS_REGION=$(curl --silent http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+export STACK_NAME=$(aws ec2 describe-instances --instance-ids $TRIFACTA_INSTANCE_ID --region $AWS_REGION |jq '.Reservations[].Instances[].Tags[] |select(.Key == "aws:cloudformation:stack-name").Value' -r)
+aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION>stack.json
+
+export TRIFACTA_URL=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "TrifactaUrl").OutputValue' -r)
+export TRIFACTA_BUCKET=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "TrifactaBucket").OutputValue' -r)
+export EMR_CLUSTER_ID=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "EmrClusterId").OutputValue' -r)
+
+curl -s -X POST -H "Content-Type: application/json" -d '{"lifetimeSeconds":86400, "description":"Autolaunch 1 day token"}' -u admin@trifacta.local:$TRIFACTA_INSTANCE_ID $TRIFACTA_URL/v4/apiAccessTokens >token.json
+export TRIFACTA_TOKEN=$(cat token.json | jq '.tokenValue' -r)
+
+# Set aws.s3.enabled to true using configuration service
+curl -s -X PUT -H 'Content-Type: application/json' --data 'true' localhost:10075/v1/setting/aws.s3.enabled/system/1
+
+# Update triconf
+python3 trifacta-autolaunch/update_triconf.py
+service trifacta restart
+
+# Configure EMR
+curl -s -X POST -H "Content-Type: application/json" -d "{\"emrClusterId\": \"$EMR_CLUSTER_ID\", \"resourceBucket\": \"$TRIFACTA_BUCKET\", \"resourcePath\": \"resources\"}" -H "Authorization: Bearer $TRIFACTA_TOKEN" $TRIFACTA_URL/v4/emrClusters
+
+echo "All done!"
