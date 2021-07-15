84639928777f07f4dca0b05e6799aca0246d4acd
vbalasu <vbalasu@gmail.com>
2021-07-14 21:44:34 -0700

    Added -s to curl commands, fixed typo in variable name

--- autolaunch.sh
+++ autolaunch.sh
@@ -9,11 +9,11 @@ export TRIFACTA_INSTANCE_ID=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.
 export TRIFACTA_BUCKET=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "TrifactaBucket").OutputValue' -r)
 export EMR_CLUSTER_ID=$(cat stack.json |jq '.Stacks[].Outputs[] | select(.OutputKey == "EmrClusterId").OutputValue' -r)
 export TRIFACTA_PUBLIC_DNS_NAME=$(aws ec2 describe-instances --instance-ids $TRIFACTA_INSTANCE_ID |jq '.Reservations[].Instances[].PublicDnsName' -r)
-curl -X POST -H "Content-Type: application/json" -d '{"lifetimeSeconds":86400, "description":"Autolaunch 1 day token"}' -u admin@trifacta.local:$TRIFACTA_INSTANCE_ID $TRIFACTA_URL/v4/apiAccessTokens >token.json
+curl -s -X POST -H "Content-Type: application/json" -d '{"lifetimeSeconds":86400, "description":"Autolaunch 1 day token"}' -u admin@trifacta.local:$TRIFACTA_INSTANCE_ID $TRIFACTA_URL/v4/apiAccessTokens >token.json
 export TRIFACTA_TOKEN=$(cat token.json | jq '.tokenValue' -r)
 
 # Set aws.s3.enabled to true using configuration service
-ssh -i $PATH_TO_KEY centos@$TRIFACUBLIC_DNS_NAME "curl -s -X PUT -H 'Content-Type: application/json' --data 'true' localhost:10075/v1/setting/aws.s3.enabled/system/1"
+ssh -i $PATH_TO_KEY centos@$TRIFACTA_PUBLIC_DNS_NAME "curl -s -X PUT -H 'Content-Type: application/json' --data 'true' localhost:10075/v1/setting/aws.s3.enabled/system/1"
 
 # Copy update_triconf.py to server and execute it
 scp -i $PATH_TO_KEY update_triconf.py centos@$TRIFACTA_PUBLIC_DNS_NAME:update_triconf.py
@@ -22,4 +22,6 @@ ssh -i $PATH_TO_KEY centos@$TRIFACTA_PUBLIC_DNS_NAME "sudo python3 update_tricon
 ssh -i $PATH_TO_KEY centos@$TRIFACTA_PUBLIC_DNS_NAME "sudo service trifacta restart"
 
 # Configure EMR
-curl -X POST -H "Content-Type: application/json" -d "{\"emrClusterId\": \"$EMR_CLUSTER_ID\", \"resourceBucket\": \"$TRIFACTA_BUCKET\", \"resourcePath\": \"resources\"}" -H "Authorization: Bearer $TRIFACTA_TOKEN" $TRIFACTA_URL/v4/emrClusters
+curl -s -X POST -H "Content-Type: application/json" -d "{\"emrClusterId\": \"$EMR_CLUSTER_ID\", \"resourceBucket\": \"$TRIFACTA_BUCKET\", \"resourcePath\": \"resources\"}" -H "Authorization: Bearer $TRIFACTA_TOKEN" $TRIFACTA_URL/v4/emrClusters
+
+echo "All done!"
